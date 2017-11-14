698782cfc67cc1d5718348544cf23a6ee4eee167
coinforge1 <33655043+coinforge1@users.noreply.github.com>
2017-11-14 08:16:20 -0200

    atualização do script

--- auto-click-bitfarm.js
+++ auto-click-bitfarm.js
@@ -1,18 +1,28 @@
-var myLinks = [];
- 
-$("#right a").each( function() {
-    myLinks.push( this );
-});
+(function() {
+    'use strict';
 
-var clickLink = function(){
-	if(myLinks.length > 0){
-		var url = $(myLinks.pop()).attr("href");
-		console.log("clickando em " + url);
-		var propaganda = window.open(url);
-		setInterval(function(){propaganda.close();}, 40000);
-	}else{
-		console.log("finalizado!!");
-	}
-}
+    var myLinks = [];
 
-setInterval(clickLink, 42000);
+    $("#right a").each( function() {
+        if($(this).find(".disabled_pbx").length === 0){
+            myLinks.push( this );
+        }
+    });
+
+    var totalLinks = myLinks.length;
+
+    var clickLink = function(){
+        if(myLinks.length > 0){
+            var rank = totalLinks - myLinks.length;
+            var url = $(myLinks.pop()).attr("href");
+            console.log(rank + " de " + totalLinks + " :: clickando em " + url);
+            var propaganda = window.open(url);
+            setInterval(function(){propaganda.close();}, 40000);
+        }else{
+            console.log("finalizado!!");
+        }
+    };
+
+    console.log("Començando a clicação.");
+    setInterval(clickLink, 42000);
+})();
