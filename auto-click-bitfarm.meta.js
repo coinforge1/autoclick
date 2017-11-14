e79accbaccb40f6e46728f19f664614662f29a36
coinforge1 <33655043+coinforge1@users.noreply.github.com>
2017-11-14 20:48:17 -0200

    Scripts de instalação automática
    Separação de meta e user scripts para que a instalação ocorra no clique.

---
+++ auto-click-bitfarm.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         autoclick
+// @namespace    https://github.com/coinforge1/autoclick
+// @version      1
+// @description  Realiza o precesso de clicks automáticamente
+// @author       CoinForge1
+// @match        http://bitcofarm.com/ads
+// @match        http://bitcomine.net/ads
+// @match        http://ethero.net/ads
+// @updateURL    https://github.com/coinforge1/autoclick/raw/master/auto-click-bitfarm.user.js
+// @downloadURL  https://github.com/coinforge1/autoclick/raw/master/auto-click-bitfarm.user.js
+// @supportURL   https://github.com/coinforge1/autoclick/issues
+// @connect      *
+// @grant        none
+// ==/UserScript==
